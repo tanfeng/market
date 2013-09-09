84749a9e0b907839c46ee08fdac6ddc7d1a77ade
Steven Tan <tanlover@gmail.com>
2013-09-09 18:32:54 +0800

    add location message

--- app/controllers/weixin_validate_controller.rb
+++ app/controllers/weixin_validate_controller.rb
@@ -111,6 +111,8 @@ include REXML
 
     ret_item = Element.new("item")
 
+
+
     ret_item_title = Element.new("Title")
     ret_item_title.add_text(CData.new("朝阳大悦城-bj"))
     ret_item.add_element(ret_item_title)
